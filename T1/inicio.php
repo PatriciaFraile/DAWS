9dcce49960e89be032638a39f34ad5517c9e8722
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2023-10-23 20:21:07 +0200

    subir

--- T1/inicio.php
+++ T1/inicio.php
@@ -1,10 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-function HolaMundo()
-{
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
 
-    echo "Hola Mundo";
-}
-HolaMundo();
+<body>
 
-?>
+    <div class="mb-3">
+        <label id=name>User Name</label>
+        <input type="text" class="form-control" id="user" placeholder="UserName">
+    </div>
+    <div class="mb-3">
+        <label id=contra>Password</label>
+        <input type="password" class="form-control" id="contras" placeholder="PassWord">
+    </div>
+    <div class="mb-3">
+        <button>Register</button>
+    </div>
+    <?php
+    $usuario = "root";
+    $password = "";
+    $servidor = "localhost";
+    $basededatos = "for1";
+    $conexion = mysqli_connect($servidor, $usuario, "") or die("Error con la base de datos");
+    $db = mysqli_select_db($conexion, $basededatos) or die("Error de conexion");
+
+    ?>
+</body>
+
+</html>
