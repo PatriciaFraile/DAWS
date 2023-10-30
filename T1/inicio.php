8cb7f53e6aef1e90d51f2d87c878fbb2308263b7
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2023-10-30 19:11:22 +0100

    subir

--- T1/inicio.php
+++ T1/inicio.php
@@ -1,34 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+$usuario = "root";
+$password = "";
+$servidor = "localhost";
+$basededatos = "for1";
+// conexion a la base de datos 
+$conexion = mysqli_connect($servidor, $usuario, "") or die("Error con la base de datos");
+$db = mysqli_select_db($conexion, $basededatos) or die("Error de conexion");
+//El metodo POST es para recuperar 
+$nombre = $_POST['nombre']; //nombre--> name="nombre en el html
+$sql = "INSERT INTO datos VALUES ('$nombre')";
+$ejecutar = mysqli_query($conexion, $sql);
+if (!$ejecutar) {
+    echo "hubo algún error";
+} else {
+    echo "dato guardado";
+}
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario</title>
-</head>
-
-<body>
-
-    <div class="mb-3">
-        <label id=name>User Name</label>
-        <input type="text" class="form-control" id="user" placeholder="UserName">
-    </div>
-    <div class="mb-3">
-        <label id=contra>Password</label>
-        <input type="password" class="form-control" id="contras" placeholder="PassWord">
-    </div>
-    <div class="mb-3">
-        <button>Register</button>
-    </div>
-    <?php
-    $usuario = "root";
-    $password = "";
-    $servidor = "localhost";
-    $basededatos = "for1";
-    $conexion = mysqli_connect($servidor, $usuario, "") or die("Error con la base de datos");
-    $db = mysqli_select_db($conexion, $basededatos) or die("Error de conexion");
-
-    ?>
-</body>
-
-</html>
+?>
