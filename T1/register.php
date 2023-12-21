5d17a9241443fee795272c4143f279ac6db3585f
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2023-12-21 19:30:05 +0100

    subir

--- T1/register.php
+++ T1/register.php
@@ -10,22 +10,42 @@
 </head>
 
 <body>
-    <form action="./utils/php/conexion.php" method="post">
-        <label for="nombre">User Name</label>
-        <input type="text" class="form-control" id="nombre" name="nombre" required />
-        <label for="apellido">Second Name</label>
-        <input type="text" class="form-control" id="apellido" name="apellido" required />
-        <label for="email">Email</label>
-        <input type="email" class="form-control" id="email" name="email" required />
-        <label for="direccion">Direction</label>
-        <input type="text" class="form-control" id="direccion" name="direccion" required />
-        <label for="localidad">Location</label>
-        <input type="text" class="form-control" id="localidad" name="localidad" required />
-        <label id="contrasena">Password</label>
-        <input type="password" class="form-control" id="contrasena" name="contrasena" placeholder="PassWord" />
+    <div class="formato">
+        <form action="./utils/php/conexion.php" method="post">
+            <label for="nombre">User Name</label>
+            <input type="text" class="form-control" id="nombre" name="nombre" placeholder="user name" required />
+            <br>
+            <br>
+            <label for="apellido">Second Name</label>
+            <input type="text" class="form-control" id="apellido" name="apellido" placeholder="second name" required />
+            <br>
+            <br>
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="email" required />
+            <br>
+            <br>
+            <label for="direccion">Direction</label>
+            <input type="text" class="form-control" id="direccion" name="direccion" placeholder="direction" required />
+            <br>
+            <br>
+            <label for="localidad">Location</label>
+            <input type="text" class="form-control" id="localidad" name="localidad" placeholder="location" required />
+            <br>
+            <br>
 
-        <input type="submit" value="registrar" />
-    </form>
+            <label id="contrasena">Password</label>
+            <input type="password" class="form-control" id="contrasena" name="contrasena" placeholder="passWord"
+                required />
+            <br>
+            <br>
+
+
+            <input type="submit" value="register" />
+        </form>
+    </div>
+
+
+
 </body>
 
 </html>
